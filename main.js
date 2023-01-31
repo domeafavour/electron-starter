34df02aa68fa071a3a6e5ab308d89635fef64274
zhanghaojie <1455123087@qq.com>
2023-01-31 18:42:28 +0800

    first page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile('./index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
